708774ac8035576205eae8eb717828acebc4a1bd
JiganSav <saveliy.jigan@rambler.ru>
2023-03-09 23:31:47 +0300

    wip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Button from '../Button/Button';
 import {useTelegram} from '../../hooks/useTelegram';
 import ExchangeRates from '../ExchangeRates';
+import account from "../../../images/account_icon.svg";
 
 const Header = (props) => {
 
   const {tg, user, onClose } = useTelegram();
-  tg.BackButton.isVisible = true
+  tg.BackButton.isVisible = true;
+  tg.WebAppUser.photo_url = account
+
 
   return (
     <header
