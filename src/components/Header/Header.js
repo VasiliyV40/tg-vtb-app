370e291a0a5fb7cfd9b8821b0c409399bde5bf1f
JiganSav <saveliy.jigan@rambler.ru>
2023-03-10 11:47:13 +0300

    wip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,26 @@ import React from 'react';
 import Button from '../Button/Button';
 import {useTelegram} from '../../hooks/useTelegram';
 import ExchangeRates from '../ExchangeRates';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 
 const Header = (props) => {
 
   const {tg, user, onClose } = useTelegram();
+  let location = useLocation();
   const navigate = useNavigate();
-  tg.BackButton.isVisible = true;
-  tg.BackButton.onClick(() =>navigate(-1));
+
+  if(location.pathname === "/"){
+    tg.BackButton.isVisible = false;
+  } else {
+    tg.BackButton.isVisible = true;
+    tg.BackButton.onClick(() =>navigate(-1));
+  }
+  
 
   const aaa = () => {
-    console.log("FFFFFF => ");
     navigate(-1)
+    console.log("FFFFFF =>", location)
   }
 
 
